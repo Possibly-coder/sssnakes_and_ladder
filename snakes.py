68a0a7397cbc525b4e00e86d6748b840f80fe0d9
Pawankumar <pawankumar.gaikwad@coverstack.in>
2025-04-17 16:20:40 +0530

    added new coordinates

--- snakes.py
+++ snakes.py
@@ -5,23 +5,39 @@ current_position_of_players = {1:0, 2:0, 3:0}
 class Player:
     def __init__(self, name, length):
         self.name = f"player_{name}"
-        self.pos =0
+        self.pos = 0
+        self.grid_length = length
         self.pos_history = []
         self.dice_history = []
         self.grid_size = length*length
         self.winner = "Nope"
+        self.two_dimension_coordinates = []
 
 
     def move(self, dice_val):
         new_position = int(self.pos)+dice_val
         if new_position > self.grid_size:
+            self.pos_history.append(self.pos)
+            co_ordinates = get_coordinates(self.pos, self.grid_length)
+            self.two_dimension_coordinates.append(co_ordinates)
             return self.pos
         self.pos = new_position
+        co_ordinates = get_coordinates(self.pos, self.grid_length)
+        self.two_dimension_coordinates.append(co_ordinates)
         self.pos_history.append(new_position)
         return new_position
 
     def pos_reset(self):
         self.pos = 0
+
+def get_coordinates(pos, grid_length):
+    if pos == 1:
+        return (0,0)
+    row = (pos-1)%grid_length
+    col = (pos-1)//grid_length
+    if col%2 == 1:
+        row = grid_length-row-1
+    return (row, col)
 def check_for_collision_and_return_the_previous_player(player: Player, pos):
     for player, curr_pos in current_position_of_players.items():
         if pos == curr_pos:
@@ -46,8 +62,9 @@ def game_loop():
             print(f"{player.name}'s new position is {new_position} \n \n")
             if new_position == grid_size:
                 winner = True
+                co_ordinates = get_coordinates(grid_size, grid_length)
                 player.winner ="WINNER"
-                print(f"Player {player.name} is the winner")
+                print(f"Player {player.name} is the winner \n \n \n \n ")
                 break
 
             #we check the collision below
@@ -59,8 +76,8 @@ def game_loop():
     for player in players:
         print(f"Dice History of {player.name}: ", player.dice_history)
         print(f"Position History of {player.name}: ", player.pos_history)
+        print(f"the co-ordinate history for {player.name} is :", player.two_dimension_coordinates)
         print(f"is {player.name} a winner ??", player.winner, "\n\n")
-
 
 
 
